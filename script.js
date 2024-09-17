c3a118908c841f2b13423c51cbcb1ceabfe4117c
Assaf Itzikson <75263691+assaf-itzikson@users.noreply.github.com>
2024-09-17 14:33:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,47 +12,47 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('WebSocket connection established');
     };
 
-    // socket.onmessage = (event) => {
-    //     const message = JSON.parse(event.data);
-    //     if (message.type === 'userJoined') {
-    //         users.push(message.username);
-    //         updateUserList();
-    //         buzzButton.disabled = false;
-    //     } else if (message.type === 'userBuzzed') {
-    //         alert(`${message.username} buzzed in first! 4`);
-    //         users = [];
-    //         updateUserList();
-    //         buzzButton.disabled = true;
-    //     }
-    // };
+    socket.onmessage = (event) => {
+        const message = JSON.parse(event.data);
+        if (message.type === 'userJoined') {
+            users.push(message.username);
+            updateUserList();
+            buzzButton.disabled = false;
+        } else if (message.type === 'userBuzzed') {
+            alert(`${message.username} buzzed in first! 4`);
+            users = [];
+            updateUserList();
+            buzzButton.disabled = true;
+        }
+    };
 
-    // userForm.addEventListener('submit', (e) => {
-    //     e.preventDefault();
-    //     const username = usernameInput.value.trim();
-    //     if (username) {
-    //         if (socket.readyState === WebSocket.OPEN) {
-    //             socket.send(JSON.stringify({ type: 'join', username }));
-    //             usernameInput.value = '';
-    //         } else {
-    //             console.error('WebSocket is not open. ReadyState:', socket.readyState);
-    //         }
-    //     }
-    // });
+    userForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const username = usernameInput.value.trim();
+        if (username) {
+            if (socket.readyState === WebSocket.OPEN) {
+                socket.send(JSON.stringify({ type: 'join', username }));
+                usernameInput.value = '';
+            } else {
+                console.error('WebSocket is not open. ReadyState:', socket.readyState);
+            }
+        }
+    });
 
-    // buzzButton.addEventListener('click', () => {
-    //     if (socket.readyState === WebSocket.OPEN) {
-    //         socket.send(JSON.stringify({ type: 'buzz', username: usernameInput.value.trim() }));
-    //     } else {
-    //         console.error('WebSocket is not open. ReadyState:', socket.readyState);
-    //     }
-    // });
+    buzzButton.addEventListener('click', () => {
+        if (socket.readyState === WebSocket.OPEN) {
+            socket.send(JSON.stringify({ type: 'buzz', username: usernameInput.value.trim() }));
+        } else {
+            console.error('WebSocket is not open. ReadyState:', socket.readyState);
+        }
+    });
 
-    // function updateUserList() {
-    //     userList.innerHTML = '';
-    //     users.forEach(user => {
-    //         const li = document.createElement('li');
-    //         li.textContent = user;
-    //         userList.appendChild(li);
-    //     });
-    // }
+    function updateUserList() {
+        userList.innerHTML = '';
+        users.forEach(user => {
+            const li = document.createElement('li');
+            li.textContent = user;
+            userList.appendChild(li);
+        });
+    }
 });
