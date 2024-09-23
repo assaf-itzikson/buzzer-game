360a35d8aed6bd8175113b42ff3047215d441220
Assaf Itzikson <assaf.itzikson@immediate.co.uk>
2024-09-23 10:15:25 +0100

    refactoring server.js too

--- server.js
+++ server.js
@@ -1,49 +1,61 @@
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8080 });
 
-let rooms = { 'P&C\'s Team Hour': [] };
+let clients = [];
 
-server.on('connection', (socket) => {
-    socket.on('message', (message) => {
-        const data = JSON.parse(message);
-        if (data.type === 'join') {
-            rooms['P&C\'s Team Hour'].push({ username: data.username, socket });
-            broadcast('P&C\'s Team Hour', { type: 'currentUsers', users: rooms['P&C\'s Team Hour'].map(user => user.username) });
-            broadcastAdmin({ type: 'updateRooms', rooms: Object.keys(rooms) });
-        } else if (data.type === 'buzz') {
-            broadcast('P&C\'s Team Hour', { type: 'userBuzzed', username: data.username });
-            broadcast('P&C\'s Team Hour', { type: 'resetBuzz' });
-        } else if (data.type === 'queryUsers') {
-            socket.send(JSON.stringify({ type: 'currentUsers', users: rooms['P&C\'s Team Hour'].map(user => user.username) }));
-        } else if (data.type === 'deleteRoom') {
-            if (rooms['P&C\'s Team Hour']) {
-                rooms['P&C\'s Team Hour'].forEach(user => user.socket.send(JSON.stringify({ type: 'roomDeleted' })));
-                rooms['P&C\'s Team Hour'] = [];
-                broadcastAdmin({ type: 'updateRooms', rooms: Object.keys(rooms) });
-            }
-        } else if (data.type === 'queryRooms') {
-            socket.send(JSON.stringify({ type: 'updateRooms', rooms: Object.keys(rooms) }));
-        }
+wss.on('connection', (ws) => {
+    clients.push(ws);
+    console.log('New client connected');
+
+    ws.on('message', (message) => {
+        const parsedMessage = JSON.parse(message);
+        handleMessage(ws, parsedMessage);
     });
 
-    socket.on('close', () => {
-        for (let room in rooms) {
-            rooms[room] = rooms[room].filter(user => user.socket !== socket);
-            broadcast(room, { type: 'currentUsers', users: rooms[room].map(user => user.username) });
-        }
+    ws.on('close', () => {
+        clients = clients.filter(client => client !== ws);
+        console.log('Client disconnected');
     });
 });
 
-function broadcast(room, message) {
-    if (rooms[room]) {
-        rooms[room].forEach(user => user.socket.send(JSON.stringify(message)));
-    }
-}
+const handleMessage = (ws, message) => {
+    const messageHandlers = {
+        join: (msg) => {
+            ws.username = msg.username;
+            ws.room = msg.room;
+            broadcast({ type: 'currentUsers', users: getUsersInRoom(msg.room) });
+        },
+        buzz: (msg) => {
+            broadcast({ type: 'userBuzzed', username: msg.username, room: msg.room });
+        },
+        queryUsers: (msg) => {
+            ws.send(JSON.stringify({ type: 'currentUsers', users: getUsersInRoom(msg.room) }));
+        },
+        deleteRoom: (msg) => {
+            broadcast({ type: 'roomDeleted', room: msg.room });
+            clients = clients.filter(client => client.room !== msg.room);
+        },
+        queryRooms: () => {
+            const rooms = [...new Set(clients.map(client => client.room))];
+            ws.send(JSON.stringify({ type: 'updateRooms', rooms }));
+        }
+    };
 
-function broadcastAdmin(message) {
-    server.clients.forEach(client => {
+    if (messageHandlers[message.type]) {
+        messageHandlers[message.type](message);
+    } else {
+        console.error('Unknown message type:', message.type);
+    }
+};
+
+const broadcast = (message) => {
+    clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {
             client.send(JSON.stringify(message));
         }
     });
-}
+};
+
+const getUsersInRoom = (room) => {
+    return clients.filter(client => client.room === room).map(client => client.username);
+};
